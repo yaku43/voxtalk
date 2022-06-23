32d9fc22b6cc1a89a4ac6f0129886cac4da578fa
yaku43 <65063199+yaku43@users.noreply.github.com>
2022-06-24 00:12:18 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -193,7 +193,7 @@ async def on_message(message):
                     message.guild.voice_client.play(source)
         await client.process_commands(message)
         
-
+"""
 @client.event
 async def on_voice_state_update(member, before, after):
     if before.channel is None:
@@ -237,7 +237,7 @@ async def on_voice_state_update(member, before, after):
                     await member.guild.voice_client.disconnect()
                     await asyncio.sleep(0.5)
                     await after.channel.connect()
-             
+"""             
 @client.event
 async def on_command_error(ctx, error):
     orig_error = getattr(error, 'original', error)
